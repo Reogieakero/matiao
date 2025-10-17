39ebfe33ecf53b06aa6fc7fcc8164fe542881fd8
Reogie Akero P Mabawad <reogieakero.mabawad@dorsu.edu.ph>
2025-10-17 20:19:22 +0800

    home destructuring to components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import About from './pages/About'
 import './index.css'
 
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+    <Route path='/aboutus' element={<About />} />
     </Routes>
 
     
